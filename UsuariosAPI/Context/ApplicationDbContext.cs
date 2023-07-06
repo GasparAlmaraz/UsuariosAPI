7512d5dd2808315749540b8aa059b71885c7bda3
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-07-06 17:22:24 -0300

    Agregar archivos de proyecto.

---
+++ UsuariosAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
---
+++ UsuariosAPI/Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace UsuariosAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+
+    }
+}
