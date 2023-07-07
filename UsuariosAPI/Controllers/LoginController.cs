957e9c5c377f89cc785201d00ecabc19b96fb66f
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-07-07 15:56:23 -0300

    added login + token generation + edit to Usuario
    added login controller and model
    added token generation after succesfull login
    edited Usuario to have Role info

---
+++ UsuariosAPI/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosAPI.Context;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+
+        public LoginController (ApplicationDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        // POST: api/Login
+        [HttpPost]
+        public IActionResult Login(LoginUser userLogin)
+        {
+            var user = Aunthenticate(userLogin);
+
+            if(user != null)
+            {
+                var token = Generate(user);
+
+                return Ok("Usuario Logueado, token: " + token);
+            }
+
+            return NotFound("Usuario no Encontrado");
+        }
+
+        private Usuario Aunthenticate(LoginUser userLogin)
+        {
+            var user = _context.Usuarios.FirstOrDefault(u => u.Correo.ToLower() == userLogin.Correo.ToLower() 
+                && u.Password.ToLower() == userLogin.Password.ToLower());
+            return user;
+        }
+
+        private string Generate(Usuario user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Name),
+                new Claim(ClaimTypes.Email, user.Correo),
+                new Claim(ClaimTypes.Role, user.Rol),
+            };
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ UsuariosAPI/Models/LoginUser.cs
@@ -0,0 +1,8 @@
+﻿namespace UsuariosAPI.Models
+{
+    public class LoginUser
+    {
+        public string Correo { get; set; }
+        public string Password { get; set; }
+    }
+}
